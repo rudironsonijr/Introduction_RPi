e915c584f4ca9399b0b1ba900feff0c6df9f580b
Rudimar L. Ronsoni Jr <rudimar.ronsoni.jr@gmail.com>
2021-03-11 16:42:31 -0300

    Initial Commit

---
+++ Blinky/Blinky.cs
@@ -0,0 +1,42 @@
+﻿using Iot.Device.Hcsr501;
+using System;
+using System.Device.Gpio;
+using System.Threading;
+
+namespace Blinky
+{
+    public class Blinky
+    {
+        static void Main(string[] args)
+        {
+            var ledPinNumber = 11;
+
+            var lightTimeInMilliseconds = 200;
+            var dimTimeInMilliseconds = 200;
+
+            Console.WriteLine($"Let's blink a LED");
+            using (GpioController gpioController = new GpioController(PinNumberingScheme.Board))
+            {
+                gpioController.OpenPin(ledPinNumber, PinMode.Output);
+
+                Console.WriteLine($"The current Pinout Numbering Schema: {PinNumberingScheme.Board}");
+                Console.WriteLine($"GPIO pin enable for use: {ledPinNumber}");
+
+                Console.CancelKeyPress += (object sender, ConsoleCancelEventArgs consoleCancelEventArgs) =>
+                {
+                    gpioController.Dispose();
+                };
+
+                while (true)
+                {
+                    Console.WriteLine($"Red Lights on for: {lightTimeInMilliseconds}");
+                    gpioController.Write(ledPinNumber, PinValue.High);
+                    Thread.Sleep(lightTimeInMilliseconds);
+                    Console.WriteLine($"Red Lights on for: {dimTimeInMilliseconds}");
+                    gpioController.Write(ledPinNumber, PinValue.Low);
+                    Thread.Sleep(dimTimeInMilliseconds);
+                }
+            }
+        }
+    }
+}
---
+++ Hcsr04/Hcsr04_Sensor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Device.Gpio;
+using System.Threading;
+using Iot.Device.Hcsr04;
+using UnitsNet;
+
+namespace Hcsr04_Sensor
+{
+    class Hcsr04_Sensor
+    {
+        static void Main(string[] args)
+        {
+            int ledPinNumber = 13;
+
+            Console.WriteLine("Hello Hcsr04 Sample!");
+
+            using (Hcsr04 sonar = new Hcsr04(4, 17))
+            {
+                using (GpioController gpioController = new GpioController(PinNumberingScheme.Board))
+                {
+                    gpioController.OpenPin(ledPinNumber, PinMode.Output);
+
+                    Console.CancelKeyPress += (object sender, ConsoleCancelEventArgs consoleCancelEventArgs) =>
+                    {
+                        sonar.Dispose();
+                        gpioController.Dispose();
+                    };
+
+                    while (true)
+                    {
+                        if (sonar.TryGetDistance(out Length distance))
+                        {
+                            Console.WriteLine($"Distance: {distance.Centimeters} cm");
+
+                            if (distance.Centimeters < 15)
+                            {
+                                gpioController.Write(ledPinNumber, PinValue.High);
+                            }
+
+                            if (distance.Centimeters >= 15)
+                            {                                
+                                gpioController.Write(ledPinNumber, PinValue.Low);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error reading sensor");
+                        }
+
+                        Thread.Sleep(1000);
+                    }
+                }
+            }
+        }
+    }
+}
